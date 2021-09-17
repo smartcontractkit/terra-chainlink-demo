0e45614af94f08189c276c9d8c6d85e640d4b5f4
Evangelos Barakos <e.mparakos@gmail.com>
2021-09-17 17:40:41 +0300

    before staging changes

--- scripts/bash/add-bridges.sh
+++ scripts/bash/add-bridges.sh
@@ -3,7 +3,7 @@ source ./scripts/bash/common.sh
 
 echo "Adding Bridges to Chainlink node..."
 
-CL_URL="http://localhost:6691"
+CL_URL="https://ter-t-0.staging.org.devnet.tools/"
 
 login_cl "$CL_URL"
 
@@ -18,34 +18,34 @@ EOF
 
 curl -s -b ./cookiefile -d "$payload1" -X POST -H 'Content-Type: application/json' "$CL_URL/v2/bridge_types" &>/dev/null
 
-CL_URL="http://localhost:6692"
+# CL_URL="http://localhost:6692"
 
-login_cl "$CL_URL"
+# login_cl "$CL_URL"
 
-payload2=$(
-  cat <<EOF
-{
-"name": "terra-adapter2",
-"url": "http://172.17.0.1:8092/"
-}
-EOF
-)
+# payload2=$(
+#   cat <<EOF
+# {
+# "name": "terra-adapter2",
+# "url": "http://172.17.0.1:8092/"
+# }
+# EOF
+# )
 
-curl -s -b ./cookiefile -d "$payload2" -X POST -H 'Content-Type: application/json' "$CL_URL/v2/bridge_types" &>/dev/null
+# curl -s -b ./cookiefile -d "$payload2" -X POST -H 'Content-Type: application/json' "$CL_URL/v2/bridge_types" &>/dev/null
 
-CL_URL="http://localhost:6693"
+# CL_URL="http://localhost:6693"
 
-login_cl "$CL_URL"
+# login_cl "$CL_URL"
 
-payload3=$(
-  cat <<EOF
-{
-"name": "terra-adapter3",
-"url": "http://172.17.0.1:8093/"
-}
-EOF
-)
+# payload3=$(
+#   cat <<EOF
+# {
+# "name": "terra-adapter3",
+# "url": "http://172.17.0.1:8093/"
+# }
+# EOF
+# )
 
-curl -s -b ./cookiefile -d "$payload3" -X POST -H 'Content-Type: application/json' "$CL_URL/v2/bridge_types" &>/dev/null
+# curl -s -b ./cookiefile -d "$payload3" -X POST -H 'Content-Type: application/json' "$CL_URL/v2/bridge_types" &>/dev/null
 
 echo "Bridges has been added to Chainlink nodes"
